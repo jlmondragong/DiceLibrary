9580937ae2b7872a112ce2e0616c37b35093e553
joseluis-mondragon <joseluis-mondragon@joseluis-OptiPlex-9020>
2018-01-10 18:06:20 -0600

    Actualización

--- src/com/appixsoftware/DiceLibrary/Dice.java
+++ src/com/appixsoftware/DiceLibrary/Dice.java
@@ -41,7 +41,7 @@ public class Dice {
      */
     public void setCurrentValue(Integer currentValue) {
         if(currentValue>sidesNumber||currentValue<MINIMUM_VALUE){
-            throw new RuntimeException("The current value must be between "+MINIMUM_VALUE+" and "+sidesNumber);
+            throw new RuntimeException("The current value must be between (inclusive) "+MINIMUM_VALUE+" and "+sidesNumber);
         }
         this.currentValue = currentValue;
     }
