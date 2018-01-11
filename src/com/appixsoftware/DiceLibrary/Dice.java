a8a2ff61891da88336fe474df110a20999730ca7
joseluis-mondragon <joseluis-mondragon@joseluis-OptiPlex-9020>
2018-01-11 17:45:50 -0600

    Actualización

--- src/com/appixsoftware/DiceLibrary/Dice.java
+++ src/com/appixsoftware/DiceLibrary/Dice.java
@@ -10,12 +10,12 @@ public class Dice implements Serializable {
 
     private final Integer sidesNumber;
     private Integer currentValue;
-    private final Integer MINIMUM_VALUE=1;
+    private final Integer MINIMUM_VALUE = 1;
 
     public Dice(Integer sidesNumber) {
         this.sidesNumber = sidesNumber;
     }
-    
+
     /**
      *
      * @return the name of the dice, for example D4, D6, D12, D20 and so on.
@@ -42,15 +42,15 @@ public class Dice implements Serializable {
      * @param currentValue the currentValue to set
      */
     public void setCurrentValue(Integer currentValue) {
-        if(currentValue>sidesNumber||currentValue<MINIMUM_VALUE){
-            throw new RuntimeException("The current value must be between (inclusive) "+MINIMUM_VALUE+" and "+sidesNumber);
+        if (currentValue > sidesNumber || currentValue < MINIMUM_VALUE) {
+            throw new RuntimeException("The current value must be between (inclusive) " + MINIMUM_VALUE + " and " + sidesNumber);
         }
         this.currentValue = currentValue;
     }
 
     @Override
-    public String toString(){
-        return "type: "+getDiceName()+", value: "+currentValue;
+    public String toString() {
+        return "type: " + getDiceName() + ", value: " + currentValue;
     }
-    
+
 }
